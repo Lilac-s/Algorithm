3b518783a2da6424a4199a1f189c319e97ee8408
성유지 <anc9432@gmail.com>
2022-12-06 23:14:40 +0900

    boj_4673

---
+++ 202212/4673_셀프_넘버.py
@@ -0,0 +1,11 @@
+number = set(range(1, 10001))
+not_self_number = set()
+
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)
+    not_self_number.add(i)
+
+self_number = sorted(number - not_self_number)
+for i in self_number:
+    print(i)

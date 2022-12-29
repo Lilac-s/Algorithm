cd959c0a680fa0f5e500bb3ecf2ee271f2d596eb
Youji-Sung <anc9432@gmail.com>
2022-12-29 23:41:18 +0900

    boj_2851

--- 202212/2851_슈퍼_마리오.py
+++ 202212/2851_슈퍼_마리오.py
@@ -1,11 +1,11 @@
 before = 0
 after = 0
-ans = 0
+answer = 0
 
 for _ in range(10):
     mush = int(input())
     after += mush
     if abs(after - 100) <= abs(before-100):
-        ans = after
+        answer = after
     before += mush
-print(ans)
+print(answer)

645063b74d8a3fe06e9df7601c6249eb96177e87
Youji-Sung <anc9432@gmail.com>
2023-02-14 23:21:28 +0900

    boj_1264

---
+++ 2023/02/1264_모음의_개수.py
@@ -0,0 +1,11 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+while True:
+    sentence = input().lower()
+    if sentence == '#':
+        break
+
+    cnt = 0
+    for i in sentence:
+        if i in vowels:
+            cnt += 1
+    print(cnt)

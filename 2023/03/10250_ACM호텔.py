4c438a9cd0fa99f4cca1c93081e49a1cb8865feb
Youji-Sung <anc9432@gmail.com>
2023-03-26 23:09:32 +0900

    boj_10250

---
+++ 2023/03/10250_ACM호텔.py
@@ -0,0 +1,10 @@
+T = int(input())
+for _ in range(T):
+    h, w, n = map(int, input().split(" "))
+    if n%h == 0:
+        x = n//h
+        y = h
+    else:
+        x = n//h + 1
+        y = n%h
+    print(f'{y*100+x}')

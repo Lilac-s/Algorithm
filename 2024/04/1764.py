3ad39e8276f586ddedb9bd1861f14ff2ec859c08
Youji-Sung <anc9432@gmail.com>
2024-04-04 23:30:27 +0900

    boj_1764 fix

--- 2024/04/1764.py
+++ 2024/04/1764.py
@@ -1,20 +1,21 @@
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
 n, m = map(int, input().split(' '))
 heardlooked = {}
 result = []
 for _ in range(n):
-    heard = input()
+    heard = input().strip()
     heardlooked[heard] = True
 for _ in range(m):
-    looked = input()
+    looked = input().strip()
     value = heardlooked.get(looked)
     if value == None:
         continue
     else:
         result.append(looked)
 
+result.sort()
 print(len(result))
 for res in result:
     print(res)
